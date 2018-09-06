1cc499ad67688323ee9180a01b7e4d087f15f1b9
Adaming <Adaming@192.168.1.168>
2018-09-06 11:41:41 +0200

    thibault : commit client service

--- src/main/java/fr/adaming/service/IClientService.java
+++ src/main/java/fr/adaming/service/IClientService.java
@@ -6,14 +6,14 @@ import fr.adaming.model.Client;
 
 public interface IClientService {
 
-	public List<Class<Client>> getAll();
+	public List<Client> getAll();
 	
-	public Class<Client> getById(Integer id);
+	public Client getById(int id);
 	
-	public Class<Client> add(Client cl);
+	public Client add(Client cl);
 	
-	public Class<Client> update(Client cl);
+	public Client update(Client cl);
 	
-	public int delete(Client cl);
+	public int delete(int id);
 	
 }
