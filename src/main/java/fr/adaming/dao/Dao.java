765d8dbab4b795f9308a28a5aa391a4297f7428c
Adaming <Adaming@192.168.1.148>
2018-09-06 11:03:03 +0200

    claire: config

--- src/main/java/fr/adaming/dao/Dao.java
+++ src/main/java/fr/adaming/dao/Dao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -17,13 +18,15 @@ import org.springframework.stereotype.Repository;
 public class Dao<T, PK extends Serializable> implements IDao<T, Serializable> {
 	
 	/** 1 : créer un entityManagerFactory */
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+	@PersistenceContext(unitName="PU")
+	private EntityManager em;
+
 	private Class<T> type;
 
 	@Override
 	public List<Class<T>> getAll() {
 		
-		EntityManager em = emf.createEntityManager();
+		
 		CriteriaBuilder cb = em.getCriteriaBuilder();
 		CriteriaQuery<T> cr= cb.createQuery(type);
 		TypedQuery<T> tq = em.createQuery(cr);
@@ -34,13 +37,13 @@ public class Dao<T, PK extends Serializable> implements IDao<T, Serializable> {
 	@Override
 	public Class<T> getById(Serializable id) {
 		
-		EntityManager em = emf.createEntityManager();
+		
 		return (Class<T>) em.find(type, id);
 	}
 
 	@Override
 	public Class<T> add(T t) {
-		EntityManager em = emf.createEntityManager();
+		
 		em.persist(t);
 		
 		return (Class<T>) t;
@@ -48,7 +51,7 @@ public class Dao<T, PK extends Serializable> implements IDao<T, Serializable> {
 
 	@Override
 	public Class<T> update(T t) {
-		EntityManager em = emf.createEntityManager();
+		
 		em.find(type, t);
 		em.merge(t);
 		return (Class<T>) t;
@@ -56,7 +59,7 @@ public class Dao<T, PK extends Serializable> implements IDao<T, Serializable> {
 
 	@Override
 	public int delete(T t) {
-		EntityManager em = emf.createEntityManager();
+	
 		em.remove(t);
 		if(em.find(type, t)==null){
 			return 1;
