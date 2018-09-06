dd19e6b2b8e3d83c36706126611b38337225f6a6
Formation <Formation@192.168.1.145>
2018-09-06 12:23:34 +0200

    valentin commit client rest

--- src/main/java/fr/adaming/rest/ClientRestController.java
+++ src/main/java/fr/adaming/rest/ClientRestController.java
@@ -3,8 +3,11 @@ package fr.adaming.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.adaming.model.Client;
@@ -18,11 +21,35 @@ public class ClientRestController {
 	IClientService clService;
 	
 	
-	@RequestMapping(value="/listeCL",  method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value="/listeCL", method = RequestMethod.GET, produces = "application/json")
 	public List<Client> getAllClient(){
 		
 		return clService.getAll();
 	}
+	
+	@RequestMapping(value="/getCL", method = RequestMethod.GET, produces = "application/json")
+	public Client getClient(@RequestParam ("pId") int id){
+		return clService.getById(id);
+	}
+	
+	
+	@RequestMapping(value="/addCL", method = RequestMethod.POST, produces = "application/json", consumes = "application/json" )
+	public Client addClient(@RequestBody Client cl){
+		return clService.add(cl);
+	}
+	
+	
+	@RequestMapping(value="/updateCl",  method=RequestMethod.PUT, produces = "application/json", consumes = "application/json")
+	public Client updateClient(@RequestBody Client cl){
+		return clService.update(cl);
+	}
+	
+	@RequestMapping(value="deleteCL",  method = RequestMethod.DELETE)
+	public void deleteClient(@PathVariable int id){
+		clService.delete(id);
+	}
+	
+	
 	
 	
 	
