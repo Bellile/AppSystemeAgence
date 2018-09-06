e03c60825a4cb50ec2d27e9160541ca49a517942
Adaming <Adaming@192.168.1.44>
2018-09-06 12:29:11 +0200

    Noelia : rajout des @repository

--- src/main/java/fr/adaming/dao/AgenceDaoImpl.java
+++ src/main/java/fr/adaming/dao/AgenceDaoImpl.java
@@ -1,7 +1,10 @@
 package fr.adaming.dao;
 
+import org.springframework.stereotype.Repository;
+
 import fr.adaming.model.Agence;
 
+@Repository
 public class AgenceDaoImpl extends DaoGenericImpl<Agence> implements IAgenceDao {
 
 	public AgenceDaoImpl() {
--- src/main/java/fr/adaming/dao/BienImmobilierAVendreDaoImpl.java
+++ src/main/java/fr/adaming/dao/BienImmobilierAVendreDaoImpl.java
@@ -1,7 +1,10 @@
 package fr.adaming.dao;
 
+import org.springframework.stereotype.Repository;
+
 import fr.adaming.model.BienImmobilierAVendre;
 
+@Repository
 public class BienImmobilierAVendreDaoImpl extends DaoGenericImpl<BienImmobilierAVendre> implements IBienImmobilierAVendreDao{
 	
 	public BienImmobilierAVendreDaoImpl(){
