4c6af23dfeffe5754f8fe84939f11cb961fb60f6
Formation <Formation@192.168.1.145>
2018-09-06 12:09:34 +0200

    valentin commit correction proprio rest

--- src/main/java/fr/adaming/rest/ClientRestController.java
+++ src/main/java/fr/adaming/rest/ClientRestController.java
@@ -1,5 +1,29 @@
 package fr.adaming.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.model.Client;
+import fr.adaming.service.IClientService;
+
+@RestController
+@RequestMapping("/wscl")
 public class ClientRestController {
+	
+	@Autowired
+	IClientService clientService;
+	
+	
+	
+	public List<Client> getAllClient(){
+		
+		
+	}
+	
+	
+	
 
 }
