40bc90ac5b089a958ef66e5010f5cfcc38f5bee2
Adaming <Adaming@192.168.1.40>
2018-09-07 13:14:19 +0200

    Noelia : biens immo à louer controller et service + debut page

---
+++ src/main/webapp/assets/js/controllers/bienlouerController.js
@@ -0,0 +1,107 @@
+// creation des controllers du bien immo à louer
+monApp.controller("getAllBLCtrl", function($scope, blProvider) {
+
+	blProvider.getAll(function(donnees) {
+		$scope.liste = donnees;
+	});
+
+}).controller("getIdBLCtrl", function($scope, blProvider) {
+	$scope.id = undefined;
+	$scope.indice = false;
+	$scope.msg = "";
+
+	$scope.rechercher = function(input) {
+
+		blProvider.getID($scope.id, function(donnees) {
+
+			if (typeof donnees == 'object') {
+				$scope.indice = true;
+				$scope.msg = "";
+				$scope.bienlouer = donnees;
+			} else {
+				$scope.indice = false;
+				$scope.msg = "L'identifiant est incorrect";
+			}
+		})
+	}
+}).controller("addBLCtrl", function($scope, blProvider, $location) {
+	$scope.blForm = {
+			typeBien : "",
+			statut : "",
+			revenuCadastre : "",
+			description : "",
+			dateSoumission: "",
+			dateDisposition :"",
+			listeImage:"",
+			caution:"",
+			loyer:"",
+			charges: "",
+			typeBail:"",
+			garniture:"",
+			numRue:"",
+			rue:"",
+			cp:"",
+			localite:"",
+			pays:""
+		}
+
+	$scope.ajouter = function() {
+
+		blProvider.add($scope.blForm, function(donnees) {
+			if (typeof donnees == 'object') {
+				$scope.msg = "";
+				$location.path("listeLouer");
+			} else {
+				$scope.msg = "L'ajout a échoué";
+			}
+		})
+
+	}
+}).controller("delBLCtrl", function($scope, blProvider, $location) {
+	$scope.id = undefined;
+
+	$scope.supprimer = function() {
+
+		blProvider.del($scope.id, function(retour) {
+			if (retour == 'OK') {
+				$scope.msg = "";
+				$location.path("listeLouer");
+			} else {
+				$scope.msg = "La suppression a échoué";
+			}
+		})
+	}
+}).controller("updateBLCtrl", function($scope, blProvider, $location) {
+
+	$scope.blForm = {
+		id : undefined,
+		typeBien : "",
+		statut : "",
+		revenuCadastre : "",
+		description : "",
+		dateSoumission: "",
+		dateDisposition :"",
+		listeImage:"",
+		caution:"",
+		loyer:"",
+		charges: "",
+		typeBail:"",
+		garniture:"",
+		numRue:"",
+		rue:"",
+		cp:"",
+		localite:"",
+		pays:""
+	}
+
+	$scope.modifier = function() {
+		blProvider.update($scope.blForm, function(donnees) {
+			if (typeof donnees == 'object') {
+				$scope.msg = "";
+				$location.path("listeLouer");
+			} else {
+				$scope.msg = "La modification a échoué";
+			}
+		})
+	}
+});
---
+++ src/main/webapp/assets/js/services/bienlouerService.js
@@ -0,0 +1,72 @@
+//creation de mon service
+monApp.factory("blProvider", function ($http){
+	
+	//fonction pour recuperer la liste
+	function recupListe(callBack){
+		$http({
+			method: "GET",
+			url:"http://localhost:8080/Projet_AppSystemeAgence/wsBl/liste"
+		}).then(function successCallback(response){
+			callBack(response.data);
+		}, function errorCallback(response){
+			console.log("erreur : "+response.statusText);
+		});
+	}
+	
+	//fonction pour rechercher par ID
+	function searchById(id, callBack){
+		$http.get("http://localhost:8080/Projet_AppSystemeAgence/wsBl/recherche", {params: {pId:id}})
+		.then(function successCallback(response){
+			callBack(response.data);
+		}, function errorCallback(response){
+			console.log("erreur : "+response.statusText);
+		});
+	}
+	
+	//fonction pour ajouter un bl
+	function ajoutBL(blIn, callBack){
+		$http({
+			method: "POST",
+			url:"http://localhost:8080/Projet_AppSystemeAgence/wsBl/ajout",
+			data:angular.toJson(blIn),
+			headers:{'Content-Type':'application/json'}	
+		}).then(function successCallback(response){
+			callBack(response.data);
+		}, function errorCallback(response){});
+		console.log("erreur : "+response.statusText);
+	}
+	
+	//fonction pour modifier un bl
+	function modifBL(blIn, callBack){
+		$http({
+			method: "PUT",
+			url:"http://localhost:8080/Projet_AppSystemeAgence/wsBl/modif",
+			data:angular.toJson(blIn),
+			headers:{'Content-Type':'application/json'}
+		}).then(function successCallback(response){
+			callBack(response.data);
+		}, function errorCallback(response){});
+			console.log("erreur : "+response.statusText);
+	}
+	
+	//fonction pour supprimer un bl
+	function supprBL(id, callBack){
+		$http({
+			method: "DELETE",
+			url:"http://localhost:8080/Projet_AppSystemeAgence/wsBl/suppr/"+id,
+		}).then(function successCallback(response){
+			callBack(response.statusText);
+		}, function errorCallback(response){});
+			console.log("erreur : "+response.statusText);
+	}
+	
+	//retour de ma fonction factory = un objet
+	return {
+		getAll: recupListe,
+		getID: searchById,
+		add: ajoutBL,
+		update: modifBL,
+		del: supprBL
+	}
+	
+});
