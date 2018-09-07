96b23498a36a53115b59a9c15bc60b5073633f65
Adaming <Adaming@192.168.1.168>
2018-09-07 16:06:59 +0200

    thibault : commit correction agence et début contrat

---
+++ src/main/webapp/assets/js/services/ContratService.js
@@ -0,0 +1,74 @@
+//Création du service Agence à partir du webservice
+monApp.factory("coProvider", function($http) {
+	//Methode recupération de la liste
+	function affListe (callBack) {
+		$http({
+			method: "GET",
+			url: "http://localhost:8080/Projet_AppSystemeAgence/wsCo/liste"
+		}).then(function successCallback(response) {
+			callBack(response.data);
+		  }, function errorCallback(response) {
+			console.log("Erreur : " + response.statusText);
+		  });
+	}
+	
+	function rechParId(id, callBack) {
+		//Récupération de la recherche à partir du serveur
+		$http.get("http://localhost:8080/Projet_AppSystemeAgence/wsCo/recherche", {params:{pId: id}})
+			.then(function successCallback(response) {
+			callBack(response.data);
+		  }, function errorCallback(response) {
+			console.log("Erreur : " + response.statusText);
+		  });
+	}
+	
+	function ajout(aIn, callBack) {
+		//Récupération de la méthode ajout à partir du serveur
+		$http({
+			method: "POST",
+			url: "http://localhost:8080/Projet_AppSystemeAgence/wsCo/ajout",
+			data: angular.toJson(aIn),
+			headers:{'Content-Type': 'application/json'}
+		}).then(function successCallback(response) {
+				callBack(response.data);
+			}, function errorCallback(response) {
+				console.log("Erreur : " + response.statusText);
+			});
+	}
+	
+	function modif(aIn, callBack) {
+		//Récupération de la méthode ajout à partir du serveur
+		$http({
+			method: "PUT",
+			url: "http://localhost:8080/Projet_AppSystemeAgence/wsCo/modif",
+			data: angular.toJson(aIn),
+			headers:{'Content-Type': 'application/json'}
+		}).then(function successCallback(response) {
+				callBack(response.data);
+			}, function errorCallback(response) {
+				console.log("Erreur : " + response.statusText);
+				callBack(response.data);
+			});
+	}
+	
+	function suppr(id, callBack) {
+		//Récupération de la méthode ajout à partir du serveur
+		$http({
+			method: "DELETE",
+			url: "http://localhost:8080/Projet_AppSystemeAgence/wsCo/suppr/"+id,
+		}).then(function successCallback(response) {
+				callBack(response.statusText);
+			}, function errorCallback(response) {
+				console.log("Erreur : " + response.statusText);
+			});
+	}
+	
+	//Le retour de ma fonction factory
+	return {
+		getAll:affListe,
+		getId:rechParId,
+		add:ajout,
+		update:modif,
+		del:suppr
+	}
+})
