db330b4f037b9172b4e49ed9d5e9626f0d80e003
Adaming <Adaming@192.168.1.148>
2018-09-12 10:28:54 +0200

    claire: commit nouveau controller

--- src/main/webapp/assets/js/client/controllers/blPCController.js
+++ src/main/webapp/assets/js/client/controllers/blPCController.js
@@ -1,48 +1,70 @@
 // creation des controllers du bien immo à louer
 appClient.controller("findAllBLCtrl",
-		function($scope, blPCProvider, $rootScope,  $location) {
+		function($scope, blPCProvider, bvPCProvider, $rootScope, $location) {
 
 			blPCProvider.getAll(function(donnees) {
 				$scope.liste = donnees;
-				
+
+			});
+
+			bvPCProvider.getAll(function(donnees) {
+				$scope.liste1 = donnees;
 			});
 
 			$rootScope.blGetOne = undefined;
+			$rootScope.bvGetOne = undefined;
 
-			$scope.displayInfos = function(l) {
+			$scope.displayInfosbl = function(l) {
 
-				console.log("-------------------- " + $rootScope.blGetOne)
 				$rootScope.blGetOne = l;
 
-				$location.path("detail");
+				$location.path("detailBl");
+			}
+
+			$scope.displayInfosbv = function(l) {
+
+				$rootScope.bvGetOne = l;
+
+				$location.path("detailBv");
 			}
 		})
-		.controller("getIdBLCtrl", function($scope, blPCProvider,$rootScope) {
+
+.controller("getIdBLCtrl", function($scope, $rootScope) {
+	$scope.msg = "";
+
+	if ($rootScope.blGetOne != undefined) {
+		$scope.bienlouer = $rootScope.blGetOne;
+	} else {
+		$scope.msg = "404 Not found ( Bien introuvable ) "
+	}
+	
+	
+}).controller("getIdBVPCCtrl", function($scope, $rootScope) {
+	$scope.msg = "";
+
+	if ($rootScope.bvGetOne != undefined) {
+		$scope.bienvendre = $rootScope.bvGetOne;
+	} else {
+		$scope.msg = "404 Not found ( Bien introuvable ) "
+	}
+	
+}).controller("getAdresseBLCtrl", function($scope, blPCProvider) {
+	$scope.id = undefined;// creation des controllers du bien immo à louer
+	$scope.indice = false;
+	$scope.msg = "";
+
+	$scope.rechercher = function() {
+
+		blPCProvider.getAdresse($scope.adresse, function(donnees) {
+
+			if (typeof donnees == 'object') {
+				$scope.indice = true;
 				$scope.msg = "";
-				
-				if ($rootScope.blGetOne != undefined) {
-					$scope.bienlouer = $rootScope.blGetOne;
-				} else {
-					$scope.msg = "404 Not found ( Bien introuvable ) "
-				}
-		})
-		.controller("getAdresseBLCtrl", function($scope, blPCProvider) {
-				$scope.id = undefined;// creation des controllers du bien immo à louer
+				$scope.bienlouer = donnees;
+			} else {
 				$scope.indice = false;
-				$scope.msg = "";
-		
-				$scope.rechercher = function() {
-
-					blPCProvider.getAdresse($scope.adresse, function(donnees) {
-
-						if (typeof donnees == 'object') {
-							$scope.indice = true;
-							$scope.msg = "";
-							$scope.bienlouer = donnees;
-						} else {
-							$scope.indice = false;
-							$scope.msg = "L'identifiant est incorrect";
-						}
-					})
-				}
-		});
+				$scope.msg = "L'identifiant est incorrect";
+			}
+		})
+	}
+});
